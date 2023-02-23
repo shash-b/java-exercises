1ce6d3c7cd502f31fd64f1270b31d7709bf15660
Shashwat <shashwat.bhandari.14@gmail.com>
2023-02-23 10:27:44 +0800

    Update prompt

--- C1P1.java
+++ C1P1.java
@@ -4,7 +4,7 @@
 
  public class C1P1 {
     public static void main(String[] args) {
-        int number = IBIO.inputInt("Enter starting number: ");
+        int number = IBIO.inputInt("How many times? ");
 
         for (int i = 0; i < number; i++) {
             System.out.println("Shashwat");
