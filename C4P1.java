f7d6a9028327ee487425692d219c3fcfc3e808e4
Shashwat <shashwat.bhandari.14@gmail.com>
2023-02-23 15:39:29 +0800

    Finish exercise

--- C4P1.java
+++ C4P1.java
@@ -4,11 +4,27 @@
 
 public class C4P1 {
     public static void main(String[] args) {
-        int a = IBIO.inputInt("enter first number ");
-        int b = IBIO.inputInt("enter second number ");
+        int a = IBIO.inputInt("Enter first number: ");
+        int b = IBIO.inputInt("Enter second number: ");
         int num;
         do {
-            // todo
+            System.out.println("Press:  [1]  for addition");
+            System.out.println("        [2]  for multiplication");
+            System.out.println("        [3]  for quit");
+
+            num = IBIO.inputInt(">>> ");
+
+            if (num != 1 && num != 2 && num != 3)
+                System.out.println("Error - invalid option\n");
+
+            switch (num) {
+                case 1:
+                    System.out.println(a + b + "\n");
+                    break;
+                case 2:
+                    System.out.println(a * b + "\n");
+                    break;
+            }
         } while (num != 3);
 
     }
