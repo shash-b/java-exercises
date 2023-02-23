fbb70c9dda57ec6cd2ff58f188c915c143f1447a
Shashwat <shashwat.bhandari.14@gmail.com>
2023-02-23 13:52:29 +0800

    Fix output

--- C3P1.java
+++ C3P1.java
@@ -6,7 +6,7 @@ public class C3P1 {
     public static void main(String[] args) {
         int x;
         do {
-            x = IBIO.inputInt("Enter a number between 0 and 100: " );
+            x = IBIO.inputInt("Enter a number between 0 and 100 that is even: " );
         } while (x >= 100 || x <= 0 || x % 2 != 0);
     }
 }
