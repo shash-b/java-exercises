9d7a5ce551c18701850aafa09664cbac9a2a7546
Shashwat <shashwat.bhandari.14@gmail.com>
2023-02-23 11:04:51 +0800

    Add file

--- C1P1.java
+++ C1P1.java
@@ -1,13 +1,15 @@
-/* 
+/*
  * Pr 1.1
  */
 
- public class C1P1 {
-    public static void main(String[] args) {
-        int number = IBIO.inputInt("How many times? ");
-
-        for (int i = 0; i < number; i++) {
-            System.out.println("Shashwat");
-        }
-    }
+public class C1P1
+{
+	public static void main(String [] args)
+	{
+		int number = IBIO.inputInt("Enter number: ");
+		for(int i = 0; i < number; i++)
+		{
+			IBIO.output("Shashwat");
+		}
+	}
 }
