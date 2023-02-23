4ca60493c1f5de7e928d29c5223ca5f4244c9694
Shashwat <88643549+novas-coder@users.noreply.github.com>
2023-02-22 18:24:18 -0800

    Add files via upload

---
+++ C1P1.java
@@ -0,0 +1,13 @@
+/* 
+ * Pr 1.1
+ */
+
+ public class C1P1 {
+    public static void main(String[] args) {
+        int number = IBIO.inputInt("Enter starting number: ");
+
+        for (int i = 0; i < number; i++) {
+            System.out.println("Shashwat");
+        }
+    }
+}
