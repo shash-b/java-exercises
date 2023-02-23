49030ef7d5b141b654197d6c1f49cd5e607b9fa6
Shashwat <shashwat.bhandari.14@gmail.com>
2023-02-23 13:23:18 +0800

    Change style

--- C1P4.java
+++ C1P4.java
@@ -2,13 +2,10 @@
  * Pr 1.4
  */
 
-public class C1P4
-{
-	public static void main(String [] args)
-	{	
+public class C1P4 {
+	public static void main(String [] args) {	
 		int num = 1;
-		for(int i = 2; i < 102; num += i, i++)
-		{
+		for (int i = 2; i < 102; num += i, i++) {
 			IBIO.output(num);
 		}
 	}
--- C1P5.java
+++ C1P5.java
@@ -2,15 +2,11 @@
  * Pr 1.5
  */
 
-public class C1P5
-{
-	public static void main(String [] args)
-	{	
-		for(int i = 0; i < 20; i++)
-		{
+public class C1P5 {
+	public static void main(String [] args) {	
+		for (int i = 0; i < 20; i++) {
 			int num = 1;
-			for(int j = 0; j < i; j++)
-			{
+			for (int j = 0; j < i; j++) {
 				num *= 2;
 			}
 			IBIO.output(num);
