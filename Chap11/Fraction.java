677c047aa7cf89b6e6aa7f12bef933e3fbd13663
Shashwat <shashwat.bhandari.14@gmail.com>
2023-02-24 23:01:24 +0800

    Create basic fraction

--- Chap11/Fraction.java
+++ Chap11/Fraction.java
@@ -2,10 +2,17 @@
  * Ch 11
  */
 
+import java.util.Scanner; // for later use
+
 public class Fraction {
+
+    // the fields/data for the fraction
     int num;
     int den;
-    public static void main(String[] args) {
-        // todo
+
+    // constructor that creates a fraction
+    Fraction(int a, int b) {
+        num = a;
+        den = b;
     }
 }
---
+++ Chap11/FractionTest.java
@@ -0,0 +1,10 @@
+/*
+ * Ch 11
+ */
+
+public class FractionTest {
+    public static void main(String[] args) {
+        Fraction f = new Fraction(3, 4);
+        System.out.println(f.num + " / " + f.den);
+    }
+}
