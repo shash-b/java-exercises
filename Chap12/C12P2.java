5bd5e6eb8bc30de80e40c3bc660230c9870c789f
Shashwat <shashwat.bhandari.14@gmail.com>
2023-02-25 15:31:56 +0800

    Improve and shorten 12.2

--- Chap12/C12P2.java
+++ Chap12/C12P2.java
@@ -6,10 +6,10 @@ import java.io.*; // allow the program to use file routines
 
 public class C12P2 {
     public static void main(String args[]) throws IOException {
-        int count = 0;
+        /* int count = 0;
         String[] names = new String[20];
 
-        /* do {
+        do {
             names[count] = IBIO.input("Enter some words: ");
             if (names[count].length() == 0)
                 break;
@@ -26,17 +26,17 @@ public class C12P2 {
         File file = new File("temp.txt");
         FileReader fr = new FileReader(file);
         BufferedReader load = new BufferedReader(fr);
-        count = 0;
+        // count = 0;
         while (load.ready()) {
-            names[count] = load.readLine();
-            count++;
+            System.out.println(load.readLine());
+            // count++;
         }
         load.close();
 
-        for (int i = 0; i < 20; i++) {
-            if (names[i] == null)
+        /* for (int i = 0; i < 20; i++) {
+            if (names[i].() == null)
                 break;
             System.out.println(names[i]);
-        }
+        } */
     }
 }
